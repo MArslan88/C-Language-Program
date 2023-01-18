42273e43ea946d21dc33e17a9f1d9f70bdbaac63
MArslan88 <gery_maz_555@hotmail.com>
2023-01-18 08:48:53 +0500

    loop statements

---
+++ loop_statement.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+    for(int i=1 ; i<=10 ; i++){
+        printf("%d\n",i);
+    }
+
+    int a = 1;
+    while(a<=10){
+        printf("%d\n",a);
+        a++;
+    }
+
+    int b = 1;
+    do{
+        printf("%d\n",b);
+        b++;
+    }
+    while (b<=5);
+    
+
+
+
+
+    return 0;
+}
