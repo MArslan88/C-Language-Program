32bbafd7133f9669214acde808ed4144275804ba
MArslan88 <gery_maz_555@hotmail.com>
2023-03-03 08:31:13 +0500

    local and global variable

---
+++ .vscode/global_local.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sum();
+
+    int a=5;
+
+int main()
+{
+    // if(a>0){
+    //     int b = 7; 
+    //     printf("%d",a);
+    // }
+
+    // printf("%d",a);
+
+    sum();
+
+    return 0;
+}
+
+void sum(){
+    printf("%d",a);
+}
+
