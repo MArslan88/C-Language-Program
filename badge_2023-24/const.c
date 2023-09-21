2352964069d5ea39af2e46b8febaa7fba093228b
Muhammad Arslan <gery_maz_555@hotmail.com>
2023-09-21 08:13:36 +0500

    constants and scape sequence

---
+++ badge_2023-24/const.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    const double pie = 22.0/7.0;
+
+
+
+    printf("Value in variable is %f",pie);
+
+     
+
+    return 0;
+}
---
+++ badge_2023-24/scape_sequence.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("Class - 1\t");
+    printf("Class - 2\t");
+    printf("Class - 3\t");
+    printf("Class - 4\t");
+    printf("Class - 5\t");
+    printf("Class - 6\t");
+
+     
+
+    return 0;
+}
