f7b1a3db04fa44840db38a071eface8bb453616e
Muhammad Arslan <gery_maz_555@hotmail.com>
2023-09-21 08:56:58 +0500

    if statement with relational operators

---
+++ badge_2023-24/ralations.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int val_1 = 4;
+    int val_2 = 6;
+
+    /*
+    if(condition){
+        statements...
+    }
+    */
+
+    if(val_1 == val_2){
+        printf("Both values are equal\n");
+    }
+
+    if(val_1 != val_2){
+        printf("Both values are not equal.\n");
+    }
+
+    printf("Program end");
+
+     
+
+    return 0;
+}
