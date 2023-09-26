4803f11f734fecea5e1d3a25a842aa0d090f5dc1
Muhammad Arslan <gery_maz_555@hotmail.com>
2023-09-26 08:50:53 +0500

    logical operators

---
+++ badge_2023-24/logical_operators.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    // AND Operator (logically multiplication)
+    // int age = 19;
+    // int cnic = 0;
+
+    // if(age >= 18 && cnic == 1){
+    //         printf("You can cast a vote.");
+    // }else{
+    //     printf("Your age is not eligible, or visit the Nadra Office.");
+    // }
+
+    // OR Operator (Logical addition)
+
+    // int value_1 = 5;
+    // if(value_1 >10 || value_1 == 5){
+    //     printf("The required value is existing.");
+    // }else{
+    //     printf("The required value is not existing.");
+    // }
+
+
+    // NOT Operator
+
+    int val = 4;
+    if(!val>2){
+        printf("Condition is TRUE.");
+    }else{
+        printf("Condition is FALSE.");
+    }
+
+
+    return 0;
+}
