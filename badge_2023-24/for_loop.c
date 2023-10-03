6e600832818211b926c1bfb99918090401c54c83
Muhammad Arslan <gery_maz_555@hotmail.com>
2023-10-03 09:05:36 +0500

    increment and decrement

---
+++ badge_2023-24/for_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    // int num = 2;
+
+    // for(int a=1; a<5; a++){
+    //     num = num * a;
+    // }
+    // printf("the value of num is : %d",num);
+
+    // for(int a=0; a<10; a++){
+
+    // }
+
+
+
+        return 0;
+}
---
+++ badge_2023-24/increment.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    int a=1;
+    // increment
+    // 1- Post increment (a++)
+    // 2- Pre increment (++a)
+
+    // a = a+1; // a++
+    // a++;
+    printf("%d\n",a++);
+    printf("%d\n",++a);
+
+    // decrement
+    // 1- Post decrement (a--)
+    // 2- Pre decrement (--a)
+
+    int b = 5;
+
+    printf("%d\n",b--);
+    printf("%d\n",b);
+
+
+
+        return 0;
+}
