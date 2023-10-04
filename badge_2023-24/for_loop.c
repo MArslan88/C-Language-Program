a6734406fe2f1d8d8fec4f6fc0e2f67184b502b6
Muhammad Arslan <gery_maz_555@hotmail.com>
2023-10-04 10:01:11 +0500

    for loop

--- badge_2023-24/for_loop.c
+++ badge_2023-24/for_loop.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 
 int main() {
+    /*
+    a--1
+    num --2
 
-    // int num = 2;
+    a 2
+    num 4
 
-    // for(int a=1; a<5; a++){
-    //     num = num * a;
-    // }
-    // printf("the value of num is : %d",num);
+    a 3
+    num 12
 
-    // for(int a=0; a<10; a++){
+    a 4
+    num 48
 
+
+    */
+
+    int num = 2;
+
+    for(int a=1; a<5; a++){
+        num = num * a;
+    }
+    printf("the value of num is : %d",num);
+
+
+    // for(int a=1; a<=10; a++){
+    //     // statements
+    //     // printf("%d ",a);
+    //     printf("%d Potato\n",a);
     // }
 
 
