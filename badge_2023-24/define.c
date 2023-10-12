2b79224d617064ab038cda4b24c723a6dcb16d22
Muhammad Arslan <gery_maz_555@hotmail.com>
2023-10-12 10:10:40 +0500

    define

---
+++ badge_2023-24/define.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define age 10
+#define name "Arslan"
+#define pi 3.14
+
+int main() {
+
+    printf("%d\n",age);
+    printf("%f\n",pi);
+    printf("%s",name);
+
+    
+
+
+
+
+
+
+
+
+    return 0;
+}
